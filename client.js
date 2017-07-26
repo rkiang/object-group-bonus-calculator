294d0ed4737482c7821474d500ee0845e303ec03
Ryan Kiang <ryanlkiang@gmail.com>
2017-07-26 15:03:00 -0500

    forked from Eric's commit

--- client.js
+++ client.js
@@ -9,4 +9,59 @@ var employees = [ atticus, jem, boo, scout, robert, mayella ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
+// console.log(employees);
+
+for (var i = 0; i < employees.length; i++) {
+
+    employees[i].annualSalary = parseInt(employees[i].annualSalary)
+
+    // console.log(employees);
+    
+
+    if (employees[i].reviewRating <= 2) {
+        employees[i].bonusPercent = 0;
+    } else if (employees[i].reviewRating == 3) {
+        employees[i].bonusPercent = 0.04;
+    } else if (employees[i].reviewRating == 4) {
+        employees[i].bonusPercent = 0.06;
+    } else if (employees[i].reviewRating == 5) {
+        employees[i].bonusPercent = 0.1;
+    }
+
+    if (employees[i].employeeNumber.length === 4) {
+        employees[i].bonusPercent = employees[i].bonusPercent +0.05
+    }
+
+    
+
+    bottomLine = employees[i].annualSalary + (employees[i].annualSalary * employees[i].bonusPercent)
+
+    if (bottomLine > 65000) {
+        employees[i].bonusPercent = employees[i].bonusPercent - 0.01
+    }
+
+    if (employees[i].bonusPercent >= 0.13) {
+        employees[i].bonusPercent = 0.13
+    } else if (employees[i].bonusPercent <= 0) {
+        employees[i].bonusPercent = 0
+    }
+
+  
+        
+   
+    
+}
+
+ for (var i = 0; i < employees.length; i++) {
+       employees[i].totalBonus = employees[i].annualSalary * employees[i].bonusPercent;
+       employees[i].totalBonus = employees[i].totalBonus.toFixed(2);
+       employees[i].totalCompensation = parseInt(employees[i].annualSalary) + parseInt(employees[i].totalBonus);
+       employees[i].totalCompensation = employees[i].totalCompensation.toFixed(2);
+
+       employees[i].totalBonus = parseInt(employees[i].totalBonus)
+       employees[i].totalCompensation = parseInt(employees[i].totalCompensation)
+
+   }
+
 console.log(employees);
+
